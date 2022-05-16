f29ac0b94a7c76d25cc1f4112004f74a33720a94
HarutoCat <104411437+HarutoCat@users.noreply.github.com>
2022-05-16 15:47:15 +0900

    Initial commit

---
+++ src/compornents/CompleteTodos.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const style = {
+  backgroundColor: "#ffffe0",
+  width: "400px",
+  minHeight: "200px",
+  padding: "8px",
+  margin: "8px",
+  borderRadius: "8px"
+};
+
+export const CompleteTodos = (props) => {
+  const { todos, onClickBack } = props;
+  return (
+    <div className="complete-area">
+      <p className="title">完了のTODO</p>
+      <ul>
+        {todos.map((done, index) => {
+          return (
+            <div key={done} style={style}>
+              <li>{done}</li>
+              <button onClick={() => onClickBack(index)}>戻す</button>
+            </div>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
